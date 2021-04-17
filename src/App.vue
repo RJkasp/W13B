5a7807987b49fdd4fd6f348eb3b75e8bc71c0fd8
rjkas <rj_kasprick@hotmail.com>
2021-04-16 20:44:56 -0600

    finished up project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click='changeLayout'>Toggle</button>
+    <section id="tweets">
+      <!-- Loop thru objects inside tweetsdata-->
+      <article v-for="tweet in tweetsData" :key="tweet">
+        <h3>{{tweet.user}}</h3>
+        <p>{{tweet.content}}</p>
+        <p>{{tweet.date}}</p>
+      </article>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return{
+      //array of tweets content,title and date
+    tweetsData: [
+      {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      },
+        {
+        content:`I’m not saying over a year in quarantine has messed with me but a tiny lizard got into my apartment and I was like, “oh good, you made it.”`,
+        user: 'RJ Kasprick',
+        date: '04/16/2021',
+      }
+    ]
+   }
+   },
+   //function for tweets and toggle thru layouts
+     methods: {
+        changeLayout: function(){
+          document.getElementById('tweets').classList.toggle('className');
+        },
+ }
 }
 </script>
 
@@ -24,5 +89,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.className{
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
 }
 </style>
